24ca9a1b1ab896f87a6e589abdf63491c16ea78d
XxHarvzBackxX <Stampylongharv@gmail.com>
2022-01-03 19:43:36 +0000

    base commit for c# stuff

---
+++ src/Hogwarts/Hogwarts/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StardewModdingAPI;
+
+/// <summary>The mod entry point.</summary>
+public class ModEntry : Mod
+{
+    /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+    /// <param name="helper">Provides simplified APIs for writing mods.</param>
+    public override void Entry(IModHelper helper)
+    {
+
+    }
+}
